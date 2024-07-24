9a469e73fbfabc4cfe4f36e773765192a76204db
emillysant <emillysantiago89@gmail.com>
2024-07-24 20:14:29 -0300

    adicionando melhorias Yuri

--- main.py
+++ main.py
@@ -8,27 +8,32 @@ import time
 
 df = pd.read_excel('challenge.xlsx')
 
-service = Service(executable_path="msedgedriver.exe")
-driver = webdriver.Edge(service=service)
+# service = Service(executable_path="msedgedriver.exe")
+# driver = webdriver.Edge(service=service)
+
+# Drive mais atualizados
+service = Service(executable_path="chromedriver.exe")
+driver = webdriver.Chrome(service=service)
 
 driver.get("https://rpachallenge.com/")
 
 def preencher_formulario(dados):
     campos = {
-        "First Name": 'input[ng-reflect-name="labelFirstName"]',
-        "Last Name": 'input[ng-reflect-name="labelLastName"]',
-        "Company Name": 'input[ng-reflect-name="labelCompanyName"]',
-        "Role in Company": 'input[ng-reflect-name="labelRole"]',
-        "Address": 'input[ng-reflect-name="labelAddress"]',
-        "Email": 'input[ng-reflect-name="labelEmail"]',
-        "Phone Number": 'input[ng-reflect-name="labelPhone"]'
+        "First Name": '//*[@ng-reflect-name="labelFirstName"]',
+        "Last Name": '//*[@ng-reflect-name="labelLastName"]',
+        "Company Name": '//*[@ng-reflect-name="labelCompanyName"]',
+        "Role in Company": '//*[@ng-reflect-name="labelRole"]',
+        "Address": '//*[@ng-reflect-name="labelAddress"]',
+        "Email": '//*[@ng-reflect-name="labelEmail"]',
+        "Phone Number": '//*[@ng-reflect-name="labelPhone"]'
     }
     
     for campo, seletor in campos.items():
         print(f"Preenchendo o campo '{campo}'...{dados[campo]}")
         try:
             input_element = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, seletor))
+                # Usando o XPATH para melhorar precisão
+                EC.presence_of_element_located((By.XPATH, seletor))
             )
             input_element.clear()
             input_element.send_keys(dados[campo])
@@ -37,17 +42,14 @@ def preencher_formulario(dados):
     
     try:
         submit_button = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, 'input[type="submit"][value="Submit"]'))
+            EC.presence_of_element_located((By.XPATH, '//input[@type="submit" and @value="Submit"]'))
         )
         submit_button.click()
     except Exception as e:
         print(f"Erro ao enviar o formulário: {e}")
 
-for index, row in df.iterrows():
-    print()
-    print(f"Processando dados do registro {index+1}...")
-    print()
-    preencher_formulario(row)
-    time.sleep(2) 
 
-driver.quit()
+if __name__ == "__main__":
+    # melhorando performace com o apply
+    df.apply(preencher_formulario, axis=1)
+    driver.quit()
